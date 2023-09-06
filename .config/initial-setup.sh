c84f27023193631a3d41267909eb01959734391d
Lee Morgan <6605757+lee-morgan@users.noreply.github.com>
2023-09-06 17:09:52 +0100

    Create initial-setup.sh

---
+++ .config/initial-setup.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash 
+
+# Post setup requirements
+repo-dir=$HOME/repos
+mkdir $repos-dir && cd $repos-dir 
+git clone https://aur.archlinux.org/paru.git 
+cd $repos-dir/paru 
+
+
+# Install software from the Arch repository 
+# I will probably do this from ansible in the future
+
+
+
+
+# Clone and setup AUR helper
+
+
+    
+
+# Install software from the AUR
+
+
+
+
+# Create required directories and files
+
+
+
+
+
+# Install Fonts
+
+
+
+
+
+
+# Enable services

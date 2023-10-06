8d79c0d1bb8d36e0b15ef2b149a1eb06bcfd03b7
Lee Morgan <lee-morgan@outlook.com>
2023-10-06 06:58:58 +0100

    Add wal templates and untracked some files

--- .cache/wal/colors-tty.sh
+++ .cache/wal/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P023110f
-                 \e]P1c04e41
-                 \e]P23fb7bc
-                 \e]P33a63ae
-                 \e]P46a51c5
-                 \e]P5ca61c1
-                 \e]P6c85959
-                 \e]P7c8c3c3
-                 \e]P85a4c4b
-                 \e]P9c04e41
-                 \e]PA3fb7bc
-                 \e]PB3a63ae
-                 \e]PC6a51c5
-                 \e]PDca61c1
-                 \e]PEc85959
-                 \e]PFc8c3c3
+    printf '%b' '\e]P0221919
+                 \e]P1ca6360
+                 \e]P24cc35c
+                 \e]P34eaec4
+                 \e]P44285c0
+                 \e]P592b8da
+                 \e]P66b9acd
+                 \e]P7c7c5c5
+                 \e]P8595252
+                 \e]P9ca6360
+                 \e]PA4cc35c
+                 \e]PB4eaec4
+                 \e]PC4285c0
+                 \e]PD92b8da
+                 \e]PE6b9acd
+                 \e]PFc7c5c5
                  \ec'
